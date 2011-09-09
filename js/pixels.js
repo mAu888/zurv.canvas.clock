bbc4f54f1f280d5c6df049bbd73460d977310846
Maurício Hanika <github.com@zurv.de>
2011-09-09 16:52:25 +0200

    Adding multiple forces to pixles example

--- js/pixels.js
+++ js/pixels.js
@@ -5,8 +5,8 @@
 		var pixels = [],
 			c = d.getElementById('canvas'),
 			context = c.getContext('2d'),
-			CIRCLES = 20,
-			SPEED = 5,
+			CIRCLES = 150,
+			SPEED = 7,
 			MIN_SPEED = .25;
 	
 		c.width = document.width;
@@ -15,10 +15,29 @@
 		var canvas = new zurv.canvas.Canvas(context, c.width, c.height);
 		
 		// force
-		var force = new zurv.canvas.Circle(new zurv.canvas.Point(c.width/2 -1, c.height/2 - 1), 10);
-		force.styles({ fill: 'rgba(255, 0, 0, .3)', stroke: 'rgba(255,0,0,.5)', width: 2 });
-		force.Q = -0.2;
-		canvas.add(force);
+		var f1 = new zurv.canvas.Circle(new zurv.canvas.Point(c.width/2 - 1, c.height/2 - 1), 10);
+		f1.styles({ fill: 'rgba(255, 0, 0, .3)', stroke: 'rgba(255,0,0,.5)', width: 2 });
+		f1.Q = -0.9;
+		
+		var f2 = new zurv.canvas.Circle(new zurv.canvas.Point(c.width/2 - 201, c.height/2 -1), 8);
+		f2.styles({ fill: 'rgba(255, 0, 0, .3)', stroke: 'rgba(255,0,0,.5)', width: 2 });
+		f2.Q = -0.8;
+		
+		var f3 = new zurv.canvas.Circle(new zurv.canvas.Point(c.width/2 + 201, c.height/2 - 1), 8);
+		f3.styles({ fill: 'rgba(255, 0, 0, .3)', stroke: 'rgba(255,0,0,.5)', width: 2 });
+		f3.Q = -0.8;
+
+		var f4 = new zurv.canvas.Circle(new zurv.canvas.Point(c.width/2 + 451, c.height/2 - 1), 5);
+		f4.styles({ fill: 'rgba(255, 0, 0, .3)', stroke: 'rgba(255,0,0,.5)', width: 2 });
+		f4.Q = -0.4;
+		
+		var f5 = new zurv.canvas.Circle(new zurv.canvas.Point(c.width/2 + - 451, c.height/2 - 1), 5);
+		f5.styles({ fill: 'rgba(255, 0, 0, .3)', stroke: 'rgba(255,0,0,.5)', width: 2 });
+		f5.Q = -0.4;
+		
+		var forces = [f1, f2, f3, f4, f5];
+		
+		canvas.add(f1, f2, f3, f4, f5);
 		
 		// particles
 		for (var i = 0; i < CIRCLES; i++) {
@@ -56,30 +75,33 @@
 					var acceleration = pixels[i].center().acceleration(),
 						radius = pixels[i].radius();
 					
-					// calculate force
-					var direction = new zurv.canvas.Vector2D(force.center().x - pixels[i].center().x, force.center().y - pixels[i].center().y),
-						distance = direction.length(),
-						actForce = (force.Q * 1000) / (distance * distance);
-
-					var length = direction.length();
-					
-					//normalize
-					if(distance > 10) {
-						direction.x = direction.x / length * actForce;
-						direction.y = direction.y / length * actForce;
+					for(var j = 0; j < forces.length; j++) {
+						var force = forces[j];
+						
+						// calculate force
+						var direction = new zurv.canvas.Vector2D(force.center().x - pixels[i].center().x, force.center().y - pixels[i].center().y),
+							distance = direction.length(),
+							actForce = (force.Q * 1000) / (distance * distance);
+						
+						//normalize
+						if(distance > 10) {
+							direction.x = direction.x / distance * actForce;
+							direction.y = direction.y / distance * actForce;
+						}
+						else {
+							direction.x = direction.x / distance * -actForce;
+							direction.y = direction.y / distance * -actForce;
+						}
+						
+						if(isNaN(direction.x)) direction.x = 0;
+						if(isNaN(direction.y)) direction.y = 0;
+						
+						acceleration.x += direction.x;
+						acceleration.y += direction.y;
+						
+						acceleration.x = (acceleration.x < 0 ? -1 : 1) * Math.min(Math.abs(acceleration.x), 10);
+						acceleration.y = (acceleration.y < 0 ? -1 : 1) * Math.min(Math.abs(acceleration.y), 10);
 					}
-					else {
-						direction.x = 0; direction.y = 0;
-					}
-					
-					if(isNaN(direction.x)) direction.x = 0;
-					if(isNaN(direction.y)) direction.y = 0;
-					
-					acceleration.x += direction.x;
-					acceleration.y += direction.y;
-					
-					acceleration.x = (acceleration.x < 0 ? -1 : 1) * Math.min(Math.abs(acceleration.x), 10);
-					acceleration.y = (acceleration.y < 0 ? -1 : 1) * Math.min(Math.abs(acceleration.y), 10);
 					
 					pixels[i].radius(Math.max(acceleration.length(), 1));
 					
